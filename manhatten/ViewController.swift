bdab32e43830556fbc66f5325cbe381cdb91d887
Ross Macmillan <ross.macmillan@live.co.uk>
2017-12-07 17:10:04 +0100

    Initial Commit

---
+++ manhatten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  manhatten
+//
+//  Created by Ross MacMillan on 07/12/2017.
+//  Copyright © 2017 Ross . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
