3fcea7ccd7bad8027c7bf2824dc04158bcdaf4da
Dennis Muensterer <dennismuensterer@aol.com>
2017-12-13 15:31:24 +0100

    fixed stuff

---
+++ manhatten/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  manhatten
+//
+//  Created by Dennis Muensterer on 13.12.17.
+//  Copyright © 2017 Ross . All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+}
